9b3d4f298a8a77a00ca83ac07798d9c6e81025bd
Doctorserious <xoxxo122@gmail.com>
2024-01-09 10:39:17 +0200

    teht1 valmis

---
+++ teht1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,b,c;
+    float avg;
+    cout << "anna 1. luku" << endl;
+    cin>>a;
+    cout << "anna 2. luku" << endl;
+    cin>>b;
+    cout << "anna 3. luku" << endl;
+    cin>>c;
+    avg=(a+b+c)/3;
+    cout<<"k-a on " <<avg<<endl;
+    return 0;
+}
