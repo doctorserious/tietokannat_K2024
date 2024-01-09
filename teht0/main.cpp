88dd93375bb83b429f5ed7775d01cee55da519a6
Doctorserious <xoxxo122@gmail.com>
2024-01-09 10:32:00 +0200

    esimerkki o valmis

--- teht0/main.cpp
+++ teht0/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    string name;
+    cout<<"Kerro nimesi"<<endl;
+    cin>>name;
+    cout<<"Terve "<<name<<endl;
     return 0;
 }
